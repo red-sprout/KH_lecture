470be0fb3f16dc4aa858235ea642bb34bad3b6ad
red-sprout <jjw05015@gmail.com>
2024-02-22 15:44:26 +0900

    2024-02-22

--- 1_java/00_testProject/src/Main.java
+++ 1_java/00_testProject/src/Main.java
@@ -1,84 +1,87 @@
 import java.io.*;
 import java.util.*;
 
-// [BOJ] 드래곤 커브 / 골드 3 / 1시간
-// 알고리즘 분류 : 구현 / 시뮬레이션
+// [BOJ] 전력난 / 골드 4 / 40분
+// 알고리즘 분류 : 그래프 이론 / 최소 스패닝 트리
 public class Main {
-	private static boolean[][] plane = new boolean[101][101];
-	private static int cnt = 0;
-	private static int gen;
-	private static int[] center;
-	private static List<int[]> points;
+	private static int m, n;
+	private static int[] parent;
 	
-	private static final int[] dc = {1, 0, -1, 0};
-	private static final int[] dr = {0, -1, 0, 1};
+	static class Road implements Comparable<Road>{
+		int x, y, cost;
+		
+		public Road(int x, int y, int cost) {
+			this.x = Math.min(x, y);
+			this.y = Math.max(x, y);
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Road n) {
+			return this.cost - n.cost;
+		}
+	}
 	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         
-        int n = Integer.parseInt(br.readLine());
-        int col, row, d, g;
-        
-        for(int i = 0; i < n; i++) {
-        	points = new ArrayList<>();
+        while(true) {
         	st = new StringTokenizer(br.readLine());
-        	col = Integer.parseInt(st.nextToken());
-        	row = Integer.parseInt(st.nextToken());
-        	d = Integer.parseInt(st.nextToken());
-        	g = Integer.parseInt(st.nextToken());
-        	
-        	gen = g;
-        	
-        	points.add(new int[] {row, col});
-        	row += dr[d];
-        	col += dc[d];
-        	
-        	center = new int[] {row, col};
-        	points.add(center);
-        	
-        	draw(0);
+        	m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            
+            if(m == 0 && n == 0) break;
+            
+            parent = new int[m];
+            Road[] roads = new Road[n];
+            
+            int x, y, z;
+            int ans = 0;
+            int distance = 0;
+            for(int i = 0; i < m; i++) {
+            	parent[i] = i;
+            }
+            
+            for(int i = 0; i < n; i++) {
+            	st = new StringTokenizer(br.readLine());
+            	x = Integer.parseInt(st.nextToken());
+            	y = Integer.parseInt(st.nextToken());
+            	z = Integer.parseInt(st.nextToken());
+            	
+            	roads[i] = new Road(x, y, z);
+            	ans += z;
+            }
+
+            Arrays.sort(roads);
+            
+            for(Road r : roads) {
+            	if(isUnion(r.x, r.y)) continue;
+            	unionParent(r.x, r.y);
+            	distance += r.cost;
+            }
+            
+            sb.append(ans - distance).append("\n");
         }
         
-        count();
-        System.out.println(cnt);
+        System.out.print(sb);
         br.close();
     }
     
-    public static void draw(int g) {
-    	if(g == gen) {
-    		for(int[] point : points) {
-    			plane[point[0]][point[1]] = true;
-    		}
-    		return;
-    	}
-    	
-    	int r0, c0, r, c;
-    	int rnext, cnext;
-    	
-    	int size = points.size();
-    	r0 = points.get(size - 1)[0];
-    	c0 = points.get(size - 1)[1];
-    	for(int i = size - 2; i >= 0; i--) {
-    		r = points.get(i)[0];
-    		c = points.get(i)[1];
-    		
-    		rnext = r0 - c0 + c;
-    		cnext = r0 + c0 - r;
-    		
-    		points.add(new int[] {rnext, cnext});
-    	}
-    	
-    	draw(g + 1);
+    public static int getParent(int a) {
+    	if(a == parent[a]) return a;
+    	return parent[a] = getParent(parent[a]);
     }
     
-    public static void count() {
-    	for(int i = 0; i < 100; i++) {
-    		for(int j = 0; j < 100; j++) {
-    			if(plane[i][j] && plane[i + 1][j] && plane[i][j + 1] && plane[i + 1][j + 1]) {
-    				cnt++;
-    			}
-    		}
-    	}
+    public static void unionParent(int a, int b) {
+    	a = getParent(a);
+    	b = getParent(b);
+    	if(a > b) parent[a] = b;
+    	else parent[b] = a;
+    }
+    
+    public static boolean isUnion(int a, int b) {
+    	return getParent(a) == getParent(b);
     }
 }
--- 2_database/05.서브쿼리(SUBQUERY).sql
+++ 2_database/05.서브쿼리(SUBQUERY).sql
@@ -52,3 +52,262 @@ WHERE SALARY >= (SELECT AVG(SALARY)
     
     >> 서브쿼리의 종류가 뭐냐에 따라서 서브쿼리 앞에 붙는 연산자 달라짐
 */
+
+/*
+    1. 단일행 서브쿼리
+    서브쿼리의 조회 결과값의 갯수가 오로지 1개일 때(한행 한열)
+    일반 비교연산자 사용 가능
+    = != > <= ...
+*/
+
+--1) 전 직원의 평균 급여보다 급여를 더 적게받는 사원들의 사원명, 직급코드, 급여 조회
+SELECT EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY < (SELECT AVG(SALARY) 
+                FROM EMPLOYEE);
+                
+--2) 최저급여를 받는 사원의 사번, 이름, 급여, 입사일 조회
+SELECT EMP_ID, EMP_NAME, SALARY, HIRE_DATE
+FROM EMPLOYEE
+WHERE SALARY = (SELECT MIN(SALARY) 
+                FROM EMPLOYEE);
+
+--3) 노옹철 사원의 급여보다 많이받는 사원들의 사번, 이름, 부서코드, 급여조회
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY > (SELECT SALARY
+                FROM EMPLOYEE
+                WHERE EMP_NAME = '노옹철');
+
+--4) 노옹철 사원의 급여보다 많이받는 사원들의 사번, 이름, 부서명, 급여조회
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, SALARY
+FROM EMPLOYEE E
+JOIN DEPARTMENT D ON (E.DEPT_CODE = D.DEPT_ID)
+WHERE SALARY > (SELECT SALARY
+                FROM EMPLOYEE
+                WHERE EMP_NAME = '노옹철');
+                
+--5) 부서별 급여합이 가장 큰 부서의 부서코드 급여합
+SELECT DEPT_CODE, SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING SUM(SALARY) = (SELECT MAX(SUM(SALARY))
+                    FROM EMPLOYEE
+                    GROUP BY DEPT_CODE);
+
+--6) '전지연'사원과 같은 부서의 사람들의 사번, 사원명, 전화번호, 입사일, 부서명을 조회
+--단, 전지연사원은 제외
+SELECT EMP_ID, EMP_NAME, PHONE, HIRE_DATE, DEPT_TITLE
+FROM EMPLOYEE E, DEPARTMENT D 
+WHERE (E.DEPT_CODE = D.DEPT_ID)
+AND (EMP_NAME != '전지연') 
+AND (DEPT_CODE = (SELECT DEPT_CODE
+                    FROM EMPLOYEE
+                    WHERE EMP_NAME = '전지연'));
+
+SELECT EMP_ID, EMP_NAME, PHONE, HIRE_DATE, DEPT_TITLE
+FROM EMPLOYEE E
+JOIN DEPARTMENT D ON (E.DEPT_CODE = D.DEPT_ID)
+WHERE (EMP_NAME != '전지연') 
+AND (DEPT_CODE = (SELECT DEPT_CODE
+                    FROM EMPLOYEE
+                    WHERE EMP_NAME = '전지연'));
+                    
+---------------------------------------------------------------------------------
+/*
+    2. 다중행 서브쿼리
+    서브쿼리를 수행한 결과값이 여러행일 때(컬럼은 한개)
+    
+    IN (서브쿼리) : 여러개의 결과값 중에서 한개라도 일치하는 값이 있다면 조회
+    > ANY (서브쿼리) : 여러개의 결과값 중에서 한개라도 클 경우
+    < ANY (서브쿼리) : 여러개의 결과값 중에서 한개라도 작을 경우 조회
+        비교대상 > ANY (서브쿼리의 결과값 => 값1, 값2, 값3...)
+        비교대상 > 값1 OR 비교대상 > 값2 OR 비교대상 > 값3
+    > ALL (서브쿼리) : 여러개의 모든 결과값들 보다 클 경우 조회
+    < ALL (서브쿼리) : 여러개의 모든 결과값들 보다 작을 경우
+        비교대상 > ALL (서브쿼리의 결과값 => 값1, 값2, 값3...)
+        비교대상 > 값1 AND 비교대상 > 값2 AND 비교대상 > 값3...
+*/
+--1) 유재식 또는 윤은해사원과 같은 직급인 사원들의 사번, 사원명, 직급코드, 급여 조회
+--1_1) 유재식 또는 윤은해사원의 직급코드
+SELECT JOB_CODE
+FROM EMPLOYEE
+WHERE EMP_NAME IN ('윤은해', '유재식');
+
+--1_2) 직급이 J3, J7인 사원들 조회
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE JOB_CODE IN ('J3', 'J7');
+
+-- 두 쿼리를 서브쿼리를 통해 합해보자
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE JOB_CODE IN (SELECT JOB_CODE
+                    FROM EMPLOYEE
+                    WHERE EMP_NAME IN ('윤은해', '유재식'));
+                    
+--2) 대리 직급임에도 과장 직급 급여들 중 최소 급여보다 많이 받는 사원들 조회
+--(사번, 이름, 직급, 급여)
+SELECT EMP_ID, EMP_NAME, JOB_NAME, SALARY
+FROM EMPLOYEE E
+JOIN JOB J ON (E.JOB_CODE = J.JOB_CODE)
+WHERE JOB_NAME = '대리'
+AND SALARY > (SELECT MIN(SALARY) 
+                FROM EMPLOYEE 
+                JOIN JOB USING (JOB_CODE)
+                WHERE JOB_NAME = '과장');
+
+SELECT EMP_ID, EMP_NAME, JOB_NAME, SALARY
+FROM EMPLOYEE E
+JOIN JOB J ON (E.JOB_CODE = J.JOB_CODE)
+WHERE JOB_NAME = '대리'
+AND SALARY > ANY (SELECT SALARY 
+                FROM EMPLOYEE 
+                JOIN JOB USING (JOB_CODE)
+                WHERE JOB_NAME = '과장');
+                
+------------------------------------------------------------------------------
+/*
+    3. 다중열 서브쿼리
+    결과값은 한 행이지만 나열된 컬럼수가 여러개일 경우
+*/
+
+--1) 하이유 사원과 같은 부서코드, 같은 직급코드에 해당하는 사원들 조회
+--(사원명, 부서코드, 직급코드, 입사일)
+--> 단일행 서브쿼리
+SELECT EMP_NAME, DEPT_CODE, JOB_CODE, HIRE_DATE
+FROM EMPLOYEE
+WHERE DEPT_CODE = (SELECT DEPT_CODE
+                    FROM EMPLOYEE
+                    WHERE EMP_NAME = '하이유')
+    AND JOB_CODE = (SELECT JOB_CODE
+                    FROM EMPLOYEE
+                    WHERE EMP_NAME = '하이유');
+---> 다중열 서브쿼리로 작성
+SELECT EMP_NAME, DEPT_CODE, JOB_CODE, HIRE_DATE
+FROM EMPLOYEE
+WHERE (DEPT_CODE, JOB_CODE) = (SELECT DEPT_CODE, JOB_CODE
+                                FROM EMPLOYEE
+                                WHERE EMP_NAME = '하이유');
+                                
+--박나라 사원과 같은 직급코드, 같은 사수를 가지고 있는 사원들의 사번, 사원명, 직급코드, 사수번호 조회
+SELECT EMP_ID, EMP_NAME, JOB_CODE, MANAGER_ID
+FROM EMPLOYEE
+WHERE (JOB_CODE, MANAGER_ID) = (SELECT JOB_CODE, MANAGER_ID
+                                FROM EMPLOYEE
+                                WHERE EMP_NAME = '박나라')
+    AND EMP_NAME = '박나라';
+
+------------------------------------------------------------------------------
+/*
+    4. 다중행 다중열 서브쿼리
+    서브쿼리의 조회 결과값이 여러행 여러열일 경우
+*/
+
+--1) 각 직급별 최소급여를 받는 사원조회(사번, 사원명, 직급코드, 급여)
+--> 각 직급별 최소급여
+SELECT JOB_CODE, MIN(SALARY)
+FROM EMPLOYEE
+GROUP BY JOB_CODE;
+
+--> 각 직급별 최소급여를 받는 사원조회(사번, 사원명, 직급코드, 급여)
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+-- 서브쿼리 적용
+WHERE (JOB_CODE, SALARY) IN (SELECT JOB_CODE, MIN(SALARY)
+                            FROM EMPLOYEE
+                            GROUP BY JOB_CODE);
+                            
+--각 부서별 최고급여를 받는 사원들의 사번, 사원명, 부서코드, 급여
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE (DEPT_CODE, SALARY) IN (SELECT DEPT_CODE, MAX(SALARY)
+                                FROM EMPLOYEE
+                                GROUP BY DEPT_CODE);
+                                
+------------------------------------------------------------------------------
+/*
+    5. 인라인 뷰
+    FROM절에 서브쿼리를 작성한 것
+    서브쿼리를 수행한 결과를 마치 테이블처럼 사용
+*/
+
+--사원들의 사번, 이름, 보너스포함연봉, 부서코드 조회
+--단, 보너스포함 연봉은 NULL이면 안된다.
+--단, 보너스포함 연봉이 3000만원 이상인 사원들만 조회
+
+SELECT ROWNUM, EMP_ID, EMP_NAME, (SALARY + (SALARY * NVL(BONUS, 0))) * 12 AS "연봉", DEPT_CODE
+FROM EMPLOYEE
+WHERE (SALARY + (SALARY * NVL(BONUS, 0))) * 12 >= 30000000
+ORDER BY 연봉 DESC;
+
+--> 인라인뷰를 주로 사용하는 예 >> TOP_N분석 : 상위 몇개만 조회
+-- 전 직원 중 금여가 가장 높은 5명만 조회
+-- ROWNUM : 오라클에서 제공해주는 컬럼, 조회된 순서대로 1부터 순번을 부여해주는 컬럼
+SELECT ROWNUM, EMP_NAME, SALARY
+FROM EMPLOYEE
+WHERE ROWNUM <= 5
+ORDER BY SALARY DESC;
+
+--> ORDER BY절이 수행된 결과를 가지고 ROWNUM 부여 -> 상위 5명 조회
+SELECT ROWNUM, EMP_NAME, SALARY
+FROM (SELECT EMP_NAME, SALARY
+        FROM EMPLOYEE
+        ORDER BY SALARY DESC)
+WHERE ROWNUM <= 5;
+
+--가장 최근에 입사한 사원 5명 조회(사원명, 급여, 입사일)
+SELECT EMP_NAME, SALARY, HIRE_DATE
+FROM (SELECT EMP_NAME, SALARY, HIRE_DATE
+        FROM EMPLOYEE
+        ORDER BY HIRE_DATE DESC)
+WHERE ROWNUM <= 5;
+
+--각 부서별 평균급여가 높은 3개의 부서 조회
+SELECT DEPT_CODE, 평균급여
+FROM (SELECT DEPT_CODE, ROUND(AVG(SALARY)) AS "평균급여"
+        FROM EMPLOYEE
+        GROUP BY DEPT_CODE
+        ORDER BY 평균급여 DESC)
+WHERE ROWNUM <= 3;
+
+--부서별 평균급여가 270만원을 초과하는 부서들에 대해서
+--(부서코드, 부서별 총 급여합, 부서별 평균급여, 부서별 사원수) 조회
+SELECT DEPT_CODE, SUM(SALARY) AS "총급여합", 
+ROUND(AVG(SALARY)) AS "평균급여", COUNT(*) AS "사원수"
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING AVG(SALARY) > 2700000
+ORDER BY DEPT_CODE ASC;
+
+SELECT *
+FROM (SELECT DEPT_CODE, SUM(SALARY) AS "총급여합", 
+        ROUND(AVG(SALARY)) AS "평균급여", COUNT(*) AS "사원수"
+        FROM EMPLOYEE
+        GROUP BY DEPT_CODE
+        ORDER BY DEPT_CODE ASC)
+WHERE 평균급여 > 2700000;
+
+------------------------------------------------------------------------
+/*
+    *순위를 매기는 함수(WINDOW FUNCTION)
+    RANK() OVER(정렬기준) | DANSE_RANK() OVER(정렬기준)
+    RANK() OVER(정렬기준) : 동일한 순위 이후의 등수를 동일한 인원 수 만큼 건너뛰고 순위 계산
+    DENSE_RANK() OVER(정렬기준) : 동일한 순위가 있다고 해도 그 다음 등수를 무조건 1씩 증가시킴
+    
+    무조건 SELECT절에서만 사용가능
+*/
+
+--급여가 높은 순서대로 순위를 매겨서 조회
+SELECT EMP_NAME, SALARY, RANK() OVER(ORDER BY SALARY DESC) AS "순위"
+FROM EMPLOYEE;
+--공동 19등 2명 그 뒤 등수는 21등으로 하나 건너 뛴 걸 알 수 있다.
+
+SELECT EMP_NAME, SALARY, DENSE_RANK() OVER(ORDER BY SALARY DESC) AS "순위"
+FROM EMPLOYEE;
+--> 19등이 공동이지만 그 뒤에 20등이 바로 나오는 것을 알 수 있다.
+
+SELECT *
+FROM (SELECT EMP_NAME, SALARY, RANK() OVER(ORDER BY SALARY DESC) AS "순위"
+        FROM EMPLOYEE)
+WHERE 순위 <= 5;
