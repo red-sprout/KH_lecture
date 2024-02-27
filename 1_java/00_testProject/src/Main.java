2792e402e844a96864aaa13d77b8122fc7d661d4
red-sprout <jjw05015@gmail.com>
2024-02-27 18:47:16 +0900

    2024-02-27 trie

--- 1_java/00_testProject/src/Main.java
+++ 1_java/00_testProject/src/Main.java
@@ -1,43 +1,69 @@
 import java.io.*;
 import java.util.*;
 
-// [BOJ] 극장 좌석 / 실버 1 / 15분
-// 알고리즘 분류 : 다이나믹 프로그래밍
+// [BOJ] 개미굴 / 골드 3 / 40분
+// 알고리즘 분류 : 자료 구조 / 문자열 / 트리 / 트라이
 public class Main {
+	private static StringBuilder sb = new StringBuilder();
+	
+	static class Node {
+		Map<String, Node> child = new TreeMap<>();
+		boolean end;
+	}
+	
+	static class Trie {
+		Node root = new Node();
+		void insert(String[] arr) {
+			Node node = this.root;
+			for(int i = 0; i < arr.length; i++) {
+				if(!node.child.containsKey(arr[i])) {
+					node.child.put(arr[i], new Node());
+				}
+				node = node.child.get(arr[i]);
+			}
+			node.end = true;
+		}
+		void search(int depth) {
+			search(depth, root);
+		}
+		void search(int depth, Node node) {
+			if(node.end == true) {
+				return;
+			}
+			
+			for(String key : node.child.keySet()) {
+				for(int i = 0; i < depth; i++) {
+					sb.append("--");
+				}
+				sb.append(key).append("\n");
+				search(depth + 1, node.child.get(key));
+			}
+		}
+	}
+	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Trie trie = new Trie();
+        StringTokenizer st;
         
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        int[] dp = new int[41];
-        List<Integer> vip = new ArrayList<>();
+        int n;
+        int k;
+        String[] arr;
         
-        dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 2;
+        n = Integer.parseInt(br.readLine());
         
-        for(int i = 3; i <= 40; i++) {
-        	dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        
-        for(int i = 0; i < m; i++) {
-        	vip.add(Integer.parseInt(br.readLine()));
-        }
-        
-        int length = 0;
-        int ans = 1;
-        for(int i = 1; i <= n; i++) {
-        	if(vip.contains(i)) {
-        		ans *= dp[length];
-        		length = 0;
-        		continue;
+        for(int i = 0; i < n; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	k = Integer.parseInt(st.nextToken());
+        	arr = new String[k];
+        	for(int j = 0; j < k; j++) {
+        		arr[j] = st.nextToken();
         	}
-        	length++;
+        	trie.insert(arr);
         }
-        
-        ans *= dp[length];
-        
-        System.out.println(ans);
+
+        trie.search(0);
+        System.out.print(sb);
         br.close();
     }
 }
