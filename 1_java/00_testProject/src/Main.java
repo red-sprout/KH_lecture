2f993e743925755d8eaccf6df6b300996d51a5c8
red-sprout <jjw05015@gmail.com>
2024-03-03 21:47:15 +0900

    2024-03-03

--- 1_java/00_testProject/src/Main.java
+++ 1_java/00_testProject/src/Main.java
@@ -1,54 +1,54 @@
 import java.io.*;
 import java.util.*;
 
-// [BOJ]  /  / 
-// 알고리즘 분류 : 
+// [BOJ] 별 찍기 11 / 골드 4 / 
+// 알고리즘 분류 : 재귀
 public class Main {
+	static char[][] star;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
         
-        int t = Integer.parseInt(br.readLine());
+        star = new char[n][2 * n];
         
-        boolean[] status1;
-        boolean[] status2;
-        
-        for(int i = 0; i < t; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int x1 = Integer.parseInt(st.nextToken());
-        	int y1 = Integer.parseInt(st.nextToken());
-        	int x2 = Integer.parseInt(st.nextToken());
-        	int y2 = Integer.parseInt(st.nextToken());
-        	
-        	int n = Integer.parseInt(br.readLine());
-        	
-        	status1 = new boolean[n];
-        	status2 = new boolean[n];
-        	
-        	for(int j = 0; j < n; j++) {
-            	st = new StringTokenizer(br.readLine());
-            	int cx = Integer.parseInt(st.nextToken());
-            	int cy = Integer.parseInt(st.nextToken());
-            	int r = Integer.parseInt(st.nextToken());
-            	
-            	int dsquare1 = (cx - x1) * (cx - x1) + (cy - y1) * (cy - y1);
-            	int dsquare2 = (cx - x2) * (cx - x2) + (cy - y2) * (cy - y2);
-            	
-            	status1[j] = dsquare1 < r * r;
-            	status2[j] = dsquare2 < r * r;
+        for(int i = 0; i < n; i++) {
+        	for(int j = 0; j < 2 * n; j++) {
+        		star[i][j] = ' ';
         	}
-        	
-        	int result = 0;
-        	for(int j = 0; j < n; j++) {
-        		if(status1[j] != status2[j]) 
-        			result++;
+        }
+        
+        int middle = n - 1;
+        draw(n, 0, middle);
+        
+        for(int i = 0; i < n; i++) {
+        	for(int j = 0; j < 2 * n; j++) {
+        		sb.append(star[i][j]);
         	}
-        	
-        	sb.append(result).append("\n");
+        	sb.append("\n");
         }
         
         System.out.print(sb);
         br.close();
+    }
+    
+    public static void draw(int n, int row, int col) {
+    	if(n == 3) {
+    		star[row][col] = '*';
+    		
+    		star[row + 1][col - 1] = '*';
+    		star[row + 1][col + 1] = '*';
+    		
+    		for(int i = 0; i < 5; i++) {
+    			star[row + 2][col - 2 + i] = '*';
+    		}
+    		
+    		return;
+    	}
+    	
+    	int next = n / 2;
+    	draw(next, row, col);
+    	draw(next, row + next, col - next);
+    	draw(next, row + next, col + next);
     }
 }
