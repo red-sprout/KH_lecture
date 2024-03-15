eee5fad0893d9e67e740fdf16c78ea9ffcacc233
red-sprout <jjw05015@gmail.com>
2024-03-15 15:44:54 +0900

    백준 2번째 과제 연습

--- 1_java/00_testProject/src/Main.java
+++ 1_java/00_testProject/src/Main.java
@@ -4,145 +4,47 @@ import java.util.*;
 // [BOJ]  /  / 
 // 알고리즘 분류 : 
 public class Main {
-	static int n, l, r;
-	static boolean[][] visited;
-	static int[][] map;
-	static List<List<int[]>> unionList = new LinkedList<>();
-	
-	static int[] dr = {1, 0, -1, 0};
-	static int[] dc = {0, 1, 0, -1};
-	
+	static char[][] board;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int day = 0;
-        boolean isMovable = true;
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        l = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-
-        visited = new boolean[n][n];
-        map = new int[n][n];
+        board = new char[n][m];
         
         for(int i = 0; i < n; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	for(int j = 0; j < n; j++) {
-        		map[i][j] = Integer.parseInt(st.nextToken());
+        	board[i] = br.readLine().toCharArray();
+        }
+        
+        int min = m * n;
+        for(int row = 0; row <= n - 8; row++) {
+        	for(int col = 0; col <= m - 8; col++) {
+        		min = Math.min(min, getBoard(row, col));
         	}
         }
         
-        while(true) {
-        	isMovable = false;
-        	for(int i = 0; i < n; i++) {
-        		for(int j = 0; j < n; j++) {
-        			if(visited[i][j]) {
-        				continue;
-        			}
-        			if(findMovablePlace(i, j)) {
-        				isMovable = true;
-        			}
-        		}
-        	}
-        	
-        	if(!isMovable) {
-        		break;
-        	}
-        	
-        	for(List<int[]> union : unionList) {
-        		setAvg(union);
-        	}
-        	
-        	for(int i = 0; i < n; i++) {
-        		for(int j = 0; j < n; j++) {
-        			visited[i][j] = false;
-        		}
-        	}
-        	
-        	unionList.clear();
-        	day++;
-        }
-        
-        System.out.println(day);
+        System.out.println(min);
         br.close();
     }
     
-    public static boolean findMovablePlace(int row, int col) {
-    	boolean result = false;
-    	int difference = 0;
-    	
-    	for(int i = 0; i < 4; i++) {
-    		int nextRow = row + dr[i];
-    		int nextCol = col + dc[i];
-    		if(nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n) {
-    			continue;
-    		}
-    		if(visited[nextRow][nextCol]) {
-    			continue;
-    		}
-    		visited[row][col] = true;
-    		difference = Math.abs(map[row][col] - map[nextRow][nextCol]);
-    		if(difference >= l && difference <= r) {
-    			result = true;
-    			bfs(row, col);
-    		}
-    	}
-    	
-    	return result;
-    }
-    
-    public static void bfs(int row, int col) {
-    	List<int[]> union = new ArrayList<>();
-    	Queue<int[]> q = new LinkedList<>();
-    	int difference = 0;
-    	
-    	int[] first = {row, col};
-    	union.add(first);
-    	q.add(first);
-    	
-    	while(!q.isEmpty()) {
-    		int[] now = q.poll();
-    		int nowRow = now[0];
-    		int nowCol = now[1];
-    		for(int i = 0; i < 4; i++) {
-    			int nextRow = nowRow + dr[i];
-    			int nextCol = nowCol + dc[i];
-    			
-    			if(nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n) {
-        			continue;
-        		}
-    			
-        		if(visited[nextRow][nextCol]) {
-        			continue;
-        		}
-        		
-        		difference = Math.abs(map[nowRow][nowCol] - map[nextRow][nextCol]);
-        		if(difference >= l && difference <= r) {
-        			visited[nextRow][nextCol] = true;
-        			int[] next = {nextRow, nextCol};
-        			union.add(next);
-        			q.add(next);
-        		}
-    		}
-    	}
-    	
-    	unionList.add(union);
-    }
-    
-    public static int getAvg(List<int[]> union) {
+    public static int getBoard(int row, int col) {
     	int result = 0;
-    	for(int[] p : union) {
-    		result += map[p[0]][p[1]];
+    	int black = 0;
+    	int white = 0;
+    	for(int i = 0; i < 8; i++) {
+    		for(int j = 0; j < 8; j++) {
+    			if((i + j) % 2 == 0) {
+	    			black = (board[row + i][col + j] == 'W') ? black + 1 : black;
+	    			white = (board[row + i][col + j] == 'B') ? white + 1 : white;
+	    		} else {
+	    			black = (board[row + i][col + j] == 'B') ? black + 1 : black;
+	    			white = (board[row + i][col + j] == 'W') ? white + 1 : white;
+	    		}
+    		}
     	}
-    	return result / union.size();
-    }
-    
-    public static void setAvg(List<int[]> union) {
-    	int avg = getAvg(union);
-    	for(int[] p : union) {
-    		map[p[0]][p[1]] = avg;
-    	}
+    	result = Math.min(black, white);
+    	return result;
     }
 }
