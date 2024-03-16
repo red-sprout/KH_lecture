47e44c64c061cf351c9073195c8e3879e8ee81a2
red-sprout <jjw05015@gmail.com>
2024-03-17 03:14:47 +0900

    위상 정렬 임시 저장

--- 1_java/00_testProject/src/Main.java
+++ 1_java/00_testProject/src/Main.java
@@ -3,7 +3,124 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
+        StringBuilder sb = new StringBuilder();
+        MyQueue q = new MyQueue();
+        
+        String[] arr = br.readLine().split(" ");
+        int n = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+        int[] prev = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
+        MyList[] next = new MyList[n + 1];
+        
+        for(int i = 0; i < m; i++) {
+        	arr = br.readLine().split(" ");
+        	int a = Integer.parseInt(arr[0]);
+            int b = Integer.parseInt(arr[1]);
+            
+            prev[b]++;
+            
+            if(next[a] == null) {
+            	next[a] = new MyList(b);
+            } else {
+            	next[a].add(b);
+            }
+        }
+        
+        for(int i = 1; i <= n; i++) {
+        	if(prev[i] == 0) {
+        		q.add(i);
+        		visited[i] = true;
+        	}
+        }
+        
+        while(!q.isEmpty()) {
+        	int now = q.remove();
+        	MyList nowList = next[now];
+        	
+        	sb.append(now).append(" ");
+        	
+        	if(nowList == null) {
+        		continue;
+        	}
+        	
+        	for(MyList.ListNode node = nowList.head; node != null; node = node.next) {
+        		if(visited[node.data]) continue;
+        		q.add(node.data);
+        		visited[node.data] = true;
+        	}
+        }
+        
+        System.out.println(sb.toString());
+        
         br.close();
     }
+}
+
+class MyList {
+	ListNode head;
+	ListNode tail;
+	
+	class ListNode {
+		int data;
+		ListNode next = null;
+		
+		ListNode(int data) {
+			this.data = data;
+		}
+	}
+	
+	public MyList(int data) {
+		this.head = new ListNode(data);
+		this.tail = this.head;
+	}
+	
+	public void add(int data) {
+		ListNode node = new ListNode(data);
+		tail.next = node;
+		tail = node;
+	}
+}
+
+class MyQueue {
+	private int size = 0;
+	private Node front = null;
+	private Node rear = null;
+	
+	class Node {
+		int data;
+		Node prev = null;
+		Node next = null;
+		
+		Node(int data) {
+			this.data = data;
+		}
+	}
+	
+	public void add(int x) {
+		Node node = new Node(x);
+		if(size == 0) {
+			front = node;
+			rear = node;
+			size++;
+			return;
+		}
+		
+		node.prev = rear;
+		rear.next = node;
+		rear = node;
+		size++;
+	}
+	
+	public int remove() {
+		Node node = front;
+		front = node.next;
+		size--;
+		return node.data;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
 }
