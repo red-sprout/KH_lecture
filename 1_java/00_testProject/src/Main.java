b438fdabbb21fa7d53e215b686e01f3b497b1aa1
red-sprout <jjw05015@gmail.com>
2024-03-20 10:50:14 +0900

    백준 스택(배열 활용)

--- 1_java/00_testProject/src/Main.java
+++ 1_java/00_testProject/src/Main.java
@@ -1,129 +1,79 @@
 import java.io.*;
 
+class MyStack {
+	private int back;
+	private int[] list;
+	
+	private static final int SIZE = 1000;
+	
+	MyStack() {
+		this.back = 0;
+		this.list = new int[SIZE];
+	}
+	
+	public void push(int data) {
+		list[back++] = data;
+	}
+	
+	public int pop() {
+		if(back == 0) {
+			return -1;
+		}
+		int data = list[--back];
+		return data;
+	}
+	
+	public int peek() {
+		if(back == 0) {
+			return -1;
+		}
+		return list[back - 1];
+	}
+}
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringBuilder sb = new StringBuilder();
-        MyDeque deque = new MyDeque();
+
         int n = Integer.parseInt(br.readLine());
+        MyStack stack1 = new MyStack();
+        MyStack stack2 = new MyStack();
+        
+        String[] students = br.readLine().split(" ");
         for(int i = 0; i < n; i++) {
-        	String[] command = br.readLine().split(" ");
-        	switch(command[0]) {
-        	case "1":
-        		deque.one(Integer.parseInt(command[1]));
-        		break;
-        	case "2":
-        		deque.two(Integer.parseInt(command[1]));
-        		break;
-        	case "3":
-        		sb.append(deque.three()).append("\n");
-        		break;
-        	case "4":
-        		sb.append(deque.four()).append("\n");
-        		break;
-        	case "5":
-        		sb.append(deque.five()).append("\n");
-        		break;
-        	case "6":
-        		sb.append(deque.six()).append("\n");
-        		break;
-        	case "7":
-        		sb.append(deque.seven()).append("\n");
-        		break;
-        	case "8":
-        		sb.append(deque.eight()).append("\n");
-        		break;
-        	}
+        	stack2.push(Integer.parseInt(students[i]));
         }
-        System.out.print(sb.toString());
+        
+        for(int i = 0; i < n; i++) {
+        	stack1.push(stack2.pop());
+        }
+        
+        boolean flag = false;
+        for(int i = 1; i <= n; i++) {
+        	int data = 0;
+        	if((data = stack2.peek()) == i) {
+        		data = stack2.pop();
+        		continue;
+        	}
+        	
+        	while(true) {
+        		data = stack1.peek();
+        		if(data == -1) {
+        			flag = true;
+        			break;
+        		}
+        		
+        		data = stack1.pop();
+        		if(data == i) {
+        			break;
+        		}
+        		stack2.push(data);
+        	}
+        	
+        	if(flag) break;
+        }
+        System.out.println(flag ? "Sad" : "Nice");
         
         br.close();
     }
-}
-
-class MyDeque{
-	private int size;
-	private Node front;
-	private Node rear;
-	
-	class Node{
-		int data;
-		Node prev;
-		Node next;
-		
-		Node(int data){
-			this.data = data;
-			prev = null;
-			next = null;
-		}
-	}
-	
-	public void one(int x) {
-		Node node = new Node(x);
-		if(size == 0) {
-			size++;
-			front = rear = node;
-			return;
-		}
-		size++;
-		node.next = front;
-		front.prev = node;
-		front = node;
-	}
-	
-	public void two(int x) {
-		Node node = new Node(x);
-		if(size == 0) {
-			size++;
-			front = rear = node;
-			return;
-		}
-		size++;
-		node.prev = rear;
-		rear.next = node;
-		rear = node;
-	}
-	
-	public int three() {
-		if(size == 0) {
-			return -1;
-		}
-		size--;
-		Node node = front;
-		front = node.next;
-		return node.data;
-	}
-	
-	public int four() {
-		if(size == 0) {
-			return -1;
-		}
-		size--;
-		Node node = rear;
-		rear = node.prev;
-		return node.data;
-	}
-	
-	public int five() {
-		return size;
-	}
-	
-	public int six() {
-		return size == 0 ? 1 : 0;
-	}
-	
-	public int seven() {
-		if(size == 0) {
-			return -1;
-		}
-		return front.data;
-	}
-	
-	public int eight() {
-		if(size == 0) {
-			return -1;
-		}
-		return rear.data;
-	}
 }
