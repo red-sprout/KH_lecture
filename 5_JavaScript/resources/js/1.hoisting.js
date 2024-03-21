c29f1f74c4451203c1037766d9b45ea83a99a988
red-sprout <jjw05015@gmail.com>
2024-03-21 16:08:29 +0900

    자바스크립트 추가

---
+++ 5_JavaScript/resources/js/1.hoisting.js
@@ -0,0 +1,18 @@
+/**
+ * Hoisting
+ */
+
+console.log(name1);
+var name1 = "jiwon";
+console.log(name1);
+
+/**
+ * 
+ * Hoisting은 뭘까?
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상
+ * let, const
+ */
+
+console.log(name2);
+let name2 = "jiwon";
