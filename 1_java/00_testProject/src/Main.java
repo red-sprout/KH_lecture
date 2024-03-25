2885e310e679f85c0d785a649135fb440cb19fae
red-sprout <jjw05015@gmail.com>
2024-03-25 15:28:21 +0900

    비우기

--- 1_java/00_testProject/src/Main.java
+++ 1_java/00_testProject/src/Main.java
@@ -4,20 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
         
-        int numer = 1;
-        int denom = 1;
         
-        for(int i = 1; i <= k; i++) {
-        	numer *= n - i + 1;
-        	denom *= i;
-        }
         
-        System.out.println(numer / denom);  
         br.close();
     }
 }
