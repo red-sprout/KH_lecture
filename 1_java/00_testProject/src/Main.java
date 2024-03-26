522701012aae7ee78de652ef36b77ccab6df0c3a
red-sprout <jjw05015@gmail.com>
2024-03-26 17:09:33 +0900

    백준

--- 1_java/00_testProject/src/Main.java
+++ 1_java/00_testProject/src/Main.java
@@ -2,8 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int r, c;
-	private static char[][] miro;
+	private static int m, n, k;
+	private static ArrayList<Integer> areaList;
+	private static boolean[][] isNotArea;
 	
 	private static int[] dr = {-1, 1, 0, 0};
 	private static int[] dc = {0, 0, -1, 1};
@@ -12,88 +13,71 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        miro = new char[r][c];
+        StringBuilder sb = new StringBuilder();
         
-        int[] jihun = new int[2];
-        List<int[]> fireList = new ArrayList<>();
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        isNotArea = new boolean[m][n];
+        areaList = new ArrayList<>();
         
-        for(int i = 0; i < r; i++) {
-        	String row = br.readLine();
-        	for(int j = 0; j < c; j++) {
-        		miro[i][j] = row.charAt(j);
-        		switch(miro[i][j]) {
-        		case 'J':
-        			jihun[0] = i;
-        			jihun[1] = j;
-        			break;
-        		case 'F':
-        			fireList.add(new int[] {i, j});
+        for(int i = 0; i < k; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int x1 = Integer.parseInt(st.nextToken());
+        	int y1 = Integer.parseInt(st.nextToken());
+        	int x2 = Integer.parseInt(st.nextToken());
+        	int y2 = Integer.parseInt(st.nextToken());
+        	
+        	for(int y = y1; y < y2; y++) {
+        		for(int x = x1; x < x2; x++) {
+        			isNotArea[y][x] = true;
         		}
         	}
         }
         
-        int result = bfs(jihun, fireList);
-        if(result == -1) {
-        	System.out.println("IMPOSSIBLE");
-        } else {
-        	System.out.println(result);
+        for(int i = 0; i < m; i++) {
+        	for(int j = 0; j < n; j++) {
+        		if(!isNotArea[i][j]) {
+        			bfs(i, j);
+        		}
+        	}
         }
+        
+        Collections.sort(areaList);
+        
+        sb.append(areaList.size()).append("\n");
+        for(int area : areaList) {
+        	sb.append(area).append(" ");
+        }
+        
+        System.out.println(sb.toString());
         
         br.close();
     }
     
-    public static int bfs(int[] jihun, List<int[]> fireList) {
-    	int result = 0;
-    	boolean isFire;
-    	Queue<int[]> jihunQ = new LinkedList<>();
-    	Queue<int[]> fireQ = new LinkedList<>();
-    	jihunQ.add(new int[] {jihun[0], jihun[1], result});
-    	for(int[] fire : fireList) {
-    		fireQ.add(new int[] {fire[0], fire[1], result});
-    	}
+    public static void bfs(int row, int col) {
+    	int tmp = 0;
+    	Queue<int[]> q = new LinkedList<>();
     	
-    	while(!jihunQ.isEmpty()) {
-    		int[] nowJihun = jihunQ.peek();
-    		int[] nowFire = fireQ.peek();
-    		
-    		isFire = fireQ.isEmpty() ? false : nowJihun[2] > nowFire[2];
-    		
-    		System.out.println(nowFire[2] + ", " +nowJihun[2]);
-    		for(int i = 0; i < r; i++) {
-    			for(int j = 0; j < c; j++) {
-    				System.out.print(miro[i][j]);
-    			}
-    			System.out.println();
-    		}
-    		if(miro[nowJihun[0]][nowJihun[1]] == 'F') continue;
-    		
-    		if(!isFire) {
-    			nowJihun = jihunQ.remove();
-        		for(int i = 0; i < 4; i++) {
-        			int nextJihunRow = nowJihun[0] + dr[i];
-        			int nextJihunCol = nowJihun[1] + dc[i];
-        			if(nextJihunRow < 0 || nextJihunRow >= r) return nowJihun[2] + 1;
-        			if(nextJihunCol < 0 || nextJihunCol >= c) return nowJihun[2] + 1;
-        			if(miro[nextJihunRow][nextJihunCol] == '#' || miro[nextJihunRow][nextJihunCol] == 'F') continue;
-        			jihunQ.add(new int[] {nextJihunRow, nextJihunCol, nowJihun[2] + 1});
-        			miro[nextJihunRow][nextJihunCol] = 'J';
-        		}
-    		} else {
-    			nowFire = fireQ.remove();
-        		for(int i = 0; i < 4; i++) {
-        			int nextFireRow = nowFire[0] + dr[i];
-        			int nextFireCol = nowFire[1] + dc[i];
-        			if(nextFireRow < 0 || nextFireRow >= r) continue;
-        			if(nextFireCol < 0 || nextFireCol >= c) continue;
-        			if(miro[nextFireRow][nextFireCol] == '#' || miro[nextFireRow][nextFireCol] == 'F') continue;
-        			fireQ.add(new int[] {nextFireRow, nextFireCol, nowFire[2] + 1});
-        			miro[nextFireRow][nextFireCol] = 'F';
-        		}
+    	tmp++;
+    	q.add(new int[] {row, col});
+    	isNotArea[row][col] = true;
+    	
+    	while(!q.isEmpty()) {
+    		int[] now = q.remove();
+    		for(int i = 0; i < 4; i++) {
+    			int nextRow = now[0] + dr[i];
+    			int nextCol = now[1] + dc[i];
+    			
+    			if(nextRow < 0 || nextRow >= m || nextCol < 0 || nextCol >= n) continue;
+    			if(isNotArea[nextRow][nextCol]) continue;
+    			
+    			tmp++;
+    			q.add(new int[] {nextRow, nextCol});
+    			isNotArea[nextRow][nextCol] = true;
     		}
     	}
     	
-    	return -1;
+    	areaList.add(tmp);
     }
 }
