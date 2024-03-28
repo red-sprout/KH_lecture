fe0a27e1161ff3d0dc220f4dd716864393938d97
red-sprout <jjw05015@gmail.com>
2024-03-28 17:47:58 +0900

    지훈 미로와 불 미로 나누어서 해결하기(idea)

--- 1_java/00_testProject/src/Main.java
+++ 1_java/00_testProject/src/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
 	private static int r, c;
 	private static char[][] miro;
+	private static char[][] miro2;
 	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,21 +20,26 @@ public class Main {
         for(int i = 0; i < r; i++) {
         	String row = br.readLine();
         	for(int j = 0; j < c; j++) {
-        		miro[r][c] = row.charAt(j);
-        		switch(miro[r][c]) {
+        		miro[i][j] = row.charAt(j);
+        		switch(miro[i][j]) {
         		case 'J':
-        			jihun[0] = r;
-        			jihun[1] = c;
+        			jihun[0] = i;
+        			jihun[1] = j;
         			jihun[2] = 0;
         			break;
         		case 'F':
-        			fireList.add(new int[] {r, c, 0});
+        			fireList.add(new int[] {i, j, 0});
         			break;
         		}
         	}
         }
         
         int result = bfs(jihun, fireList);
+        if(result == -1) {
+        	System.out.println("IMPOSSIBLE");
+        } else {
+        	System.out.println(result);
+        }
         
         br.close();
     }
@@ -44,42 +50,70 @@ public class Main {
     	int[] dr = {-1, 1, 0, 0};
     	int[] dc = {0, 0, -1, 1};
     	
-    	Queue<int[]> jihunQ = new LinkedList<>();
+    	LinkedList<int[]> jList = new LinkedList<>();
     	Queue<int[]> fireQ = new LinkedList<>();
     	
-    	jihunQ.add(jihun);
+    	jList.add(jihun);
     	for(int[] fire : fireList) {
     		fireQ.add(fire);
     	}
     	
-    	while(!jihunQ.isEmpty()) {
-    		int[] nowJihun = jihunQ.peek();
-    		int[] nowFire;
+    	while(!jList.isEmpty()) {
+    		int[] nowJihun = jList.peekFirst();
+    		int[] nowFire = null;
     		
-    		if(fireQ.isEmpty()) {
-    			fireMode = false;
-    		} else {
-    			nowFire = fireQ.peek();
-    			fireMode = (nowJihun[2] != time) && (nowFire[2] == time);
+    		System.out.println(time);
+    		for(int i = 0; i < r; i++) {
+    			for(int j = 0; j < c; j++) {
+    				System.out.print(miro[i][j]);
+    			}
+    			System.out.println();
     		}
     		
-    		if(fireMode) {
-    			nowFire = fireQ.remove();
-    			for(int i = 0; i < 4; i++) {
-    				int nextRow = nowFire[0] + dr[i];
-    				int nextCol = nowFire[1] + dc[i];
-    				
-    				if(nextRow < 0 || nextRow >= r || nextCol < 0 || nextCol >= c) continue;
-    			}
-    		} else {
-    			nowJihun = jihunQ.remove();
-    			for(int i = 0; i < 4; i++) {
-    				int nextRow = nowJihun[0] + dr[i];
-    				int nextCol = nowJihun[1] + dc[i];
-    				
-    				if(nextRow < 0 || nextRow >= r || nextCol < 0 || nextCol >= c) continue;
-    			}
-    		}
+//    		if(fireQ.isEmpty()) {
+//    			fireMode = (nowJihun[2] != time);
+//    		} else {
+//    			nowFire = fireQ.peek();
+//    			fireMode = (nowJihun[2] != time) && (nowFire[2] == time);
+//    		}
+//    		
+//    		if(fireMode) {
+//    			nowFire = fireQ.remove();
+//    			for(int i = 0; i < 4; i++) {
+//    				int nextRow = nowFire[0] + dr[i];
+//    				int nextCol = nowFire[1] + dc[i];
+//    				
+//    				if(nextRow < 0 || nextRow >= r || nextCol < 0 || nextCol >= c) continue;
+//    				if(miro[nextRow][nextCol] == 'F' || miro[nextRow][nextCol] == '#') continue;
+//    				if(miro[nextRow][nextCol] == 'J') {
+//    					for(int j = 0; j < jList.size(); j++) {
+//    						if(jList.get(j)[0] == nextRow && jList.get(j)[1] == nextCol) {
+//    							jList.remove(j);
+//    							break;
+//    						}
+//    					}
+//    				}
+//    				
+//    				fireQ.add(new int[] {nextRow, nextCol, nowFire[2] + 1});
+//    				miro[nextRow][nextCol] = 'F';
+//    			}
+//    			
+//    			if(fireQ.peek()[2] != nowFire[2]) {
+//    				time++;
+//    			}
+//    		} else {
+//    			nowJihun = jList.pollFirst();
+//    			for(int i = 0; i < 4; i++) {
+//    				int nextRow = nowJihun[0] + dr[i];
+//    				int nextCol = nowJihun[1] + dc[i];
+//    				
+//    				if(nextRow < 0 || nextRow >= r || nextCol < 0 || nextCol >= c) return nowJihun[2] + 1;
+//    				if(miro[nextRow][nextCol] == 'J' || miro[nextRow][nextCol] == 'F' || miro[nextRow][nextCol] == '#') continue;
+//    				
+//    				jList.add(new int[] {nextRow, nextCol, nowJihun[2] + 1});
+//    				miro[nextRow][nextCol] = 'J';
+//    			}
+//    		}
     	}
     	
     	return -1;
